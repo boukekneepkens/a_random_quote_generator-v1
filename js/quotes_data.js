64d603151656f6d25084a478a3b30004d9540041
Bouke Kneepkens <boukekneepkens@gmail.com>
2019-05-01 00:14:51 +0200

    test before submitting

---
+++ js/quotes_data.js
@@ -0,0 +1,70 @@
+//This is a file with all the quotes array that stores the value in an object.
+//For overview I kept it seperate from my scrpt.js file which stores all the code. Just as we learned in the challenge with the students and their reports.
+
+let quotes = [
+  {quote: "I had a dream!",
+   source: "Martin Luther King",
+   citation: "speech",
+   year: 1963,
+   category: "politics"
+   },
+  {quote: "Life's too important to be taken seriously.",
+   source: "Oscar Wilde",
+   category: "literature"
+  },
+  {quote: "In the end we will conserve only what we love. We will love only what we understand. And we will understand only what we are thaught.",
+   source: "Baba Dioum, Singalese Poet",
+   category: "education"
+  },
+  {quote: "I love deadlines. I love the wooshing noise they make as they go by.",
+   source: "Douglas Adams",
+   citation: "mindfulness",
+   category: "literature"
+
+  },
+  {quote: "This is your life. Do what you want and do it often. If you don't like something, change it. If you don't like your job, quit. If you don't have enough time, stop watching TV. If you are looking for the love of your life, stop; they will be waiting for you when you  start doing things you love. Stop over-analysing, life is simple. All emotions are beautiful. When you eat, appreciate every last bite. Life is simple. Open your heart, mind and arms to new things and people, we are united in our differences. Ask the next person you see what their passion is and share your inspiring dream with them. Travel often; getting lost will help you find yourself. Some opportunities only come once, seize them. Life is about the people you meet and the things you create with them, so go out and start creating. Life is short, live your dream and share your passion.",
+   source: "unknown",
+   category: "mindfulness"
+  },
+  {quote: "Life is short. Buy the shoes",
+   source: "Meghan March",
+   category: "humor"
+  },
+  {quote: "don't f*ck 'm if they don't read",
+   source: "my grandma",
+   category: "humor"
+  },
+  {quote: "Learn the rules like a pro, so you can break them like an artist.",
+   source: "Pablo Picasso",
+   category: "career"
+  },
+  {quote: "I may not have gone where I intended to go, but I think I have ended up where I needed to be.",
+   source: "Douglas Adams",
+   citation: "mindfulness",
+   category: "literature"
+  },
+  {quote: "There is a theory which states that if ever anyone discovers exactly what the Universe is for and why it is here, it will instantly disappear and be replaced by something even more bizarre and inexplicable. There is another theory which states that this has already happened.",
+   source: "Douglas Adams",
+   citation: "The Hitchhiker’s Guide to the Galaxy",
+   year: 1979,
+   category: "literature"
+  },
+  {quote: "I realized we had finally arrived in he future",
+   source: "Douglas Coupland",
+   citation: "Polaroids from the dead",
+   year: 1996,
+   category: "literature"
+  },
+  {quote: "Homophobia: the fear that gay men will treat you the way you treat women.",
+   source: "Urbandictionary",
+   citation: "protest speech",
+   year: 2011,
+   category: "anti-discrimination"
+  },
+  {quote: "Always be yourself. Unless you can be a unicorn... in that case you should ALWAYS be a unicorn!",
+   source: "Elle Lothlorien",
+   citation: "Alice in wonderland",
+   year:  2013,
+   category: "humor"
+  }
+];
